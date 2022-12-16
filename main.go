826f2ecd9a7fe4c363abab303f6439f3c9beae2b
Andrea Lin <andre93.lin@gmail.com>
2022-12-16 17:23:25 -0300

    feat(bill): add user input to get name and show options

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	option, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(option)
+}
 
 func main() {
-	myBill := newBill("mario's bill")
-	myBill.updateTip(30)
-	myBill.addItem("onion soup", 4.50)
-	myBill.addItem("veg pie", 8.95)
-	myBill.addItem("toffee pudding", 4.95)
-	myBill.addItem("coffee", 3.25)
-
-	fmt.Println(myBill.format())
+	myBill := createBill()
+	promptOptions(myBill)
+	fmt.Println(myBill)
 }
